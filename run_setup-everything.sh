3d747838633a195a94a8bfaee77adf0bbe1353e0
Felipe Santos <felipecassiors@gmail.com>
2020-07-27 13:09:45 -0300

    Set up dark theme for Ubuntu 20.04

--- run_setup-everything.sh
+++ run_setup-everything.sh
@@ -72,6 +72,16 @@ function is_wsl() {
   fi
 }
 
+function is_ubuntu() {
+  local version=${1-'20.04'}
+
+  if [[ "$(cat /etc/os-release)" = *VERSION_ID=\"$version\"* ]]; then
+    return 0
+  else
+    return 1
+  fi
+}
+
 if is_wsl; then
   echo_task "Performing WSL specific steps"
 
@@ -94,4 +104,12 @@ else
 
   echo_task "Adding 'Open Code here' in Nautilus context menu"
   bash -c "$(wget -qO- https://raw.githubusercontent.com/harry-cpp/code-nautilus/master/install.sh)"
+
+  if is_ubuntu 20.04; then
+    echo_task "Setting up dark theme"
+    sudo apt install -y gnome-shell-extensions
+    gnome-extensions enable user-theme@gnome-shell-extensions.gcampax.github.com
+    gsettings set org.gnome.desktop.interface gtk-theme "Yaru-dark"
+    gsettings set org.gnome.shell.extensions.user-theme name "Yaru-dark"
+  fi
 fi
