8f42dc34553d3841cc4f6822e518a407aca56b88
Felipe Santos <felipecassiors@gmail.com>
2020-08-02 19:12:00 +0000

    Add remember params for chocolatey

--- scripts/setup-everything-windows.ps1
+++ scripts/setup-everything-windows.ps1
@@ -2,24 +2,25 @@
 
 # Self-elevate the script if required
 if (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator')) {
-    if ([int](Get-CimInstance -Class Win32_OperatingSystem | Select-Object -ExpandProperty BuildNumber) -ge 6000) {
-        $CommandLine = "-NoExit -File `"" + $MyInvocation.MyCommand.Path + "`" " + $MyInvocation.UnboundArguments
-        Start-Process -FilePath PowerShell.exe -Verb Runas -ArgumentList $CommandLine
-        Exit
-    }
+  if ([int](Get-CimInstance -Class Win32_OperatingSystem | Select-Object -ExpandProperty BuildNumber) -ge 6000) {
+    $CommandLine = "-NoExit -File `"" + $MyInvocation.MyCommand.Path + "`" " + $MyInvocation.UnboundArguments
+    Start-Process -FilePath PowerShell.exe -Verb Runas -ArgumentList $CommandLine
+    Exit
+  }
 }
 
 Write-Host "--> Installing Chocolatey" -F Blue
 # Create $PROFILE if does not exist
 if(-Not (Test-Path -Path $PROFILE)) {
-    New-Item -ItemType File -Path $PROFILE
+  New-Item -ItemType File -Path $PROFILE
 }
 # Install Chocolatey using the official snippet
-Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+iwr -useb 'https://chocolatey.org/install.ps1' | iex
 choco feature enable -n=allowGlobalConfirmation
+choco feature enable -n=useRememberedArgumentsForUpgrades
 
 Write-Host "--> Installing Git" -F Blue
 choco install git --params "/NoShellIntegration /NoAutoCrlf"
 
 Write-Host "--> Installing Scoop" -F Blue
-iwr -useb get.scoop.sh | iex
+iwr -useb 'https://get.scoop.sh' | iex
