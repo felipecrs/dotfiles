9646e0dad061c44882609fd69367922be50f8bca
Felipe Santos <felipecassiors@gmail.com>
2020-10-13 14:49:47 -0300

    Fix CI and add-apt-repository not found (#21)

--- scripts/test.sh
+++ scripts/test.sh
@@ -5,6 +5,14 @@ set -euo pipefail
 script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 dotfiles_root="$(realpath "$script_dir/..")"
 
-set -x
+cmd() {
+  echo "==>" "$@"
+  "$@"
+}
 
-docker run --rm -e REMOTE_CONTAINERS=true -u vscode -v "$dotfiles_root:/home/vscode/.dotfiles:ro" -w /home/vscode/.dotfiles mcr.microsoft.com/vscode/devcontainers/base:ubuntu ./install
+echo "Remote - Containers"
+cmd docker run --rm -e REMOTE_CONTAINERS=true -u vscode -v "$dotfiles_root:/mnt/repo:ro" mcr.microsoft.com/vscode/devcontainers/base:ubuntu sh -c 'cp -r /mnt/repo ~/.dotfiles && ~/.dotfiles/install'
+
+echo
+echo "NOT Remote - Containers"
+cmd docker run --rm -u vscode -v "$dotfiles_root:/mnt/repo:ro" mcr.microsoft.com/vscode/devcontainers/base:ubuntu sh -c 'cp -r /mnt/repo ~/.dotfiles && ~/.dotfiles/install'
