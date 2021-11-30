43b8d9907619fa54ba866b9adece3589d0bec604
Felipe Santos <felipecassiors@gmail.com>
2021-11-30 17:37:44 -0300

    Improve migration

---
+++ run_migrate.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#
+# This script will only be run if chezmoi is older than 2.9.1,
+# because otherwise the .chezmoiroot file will be honored.
+
+red() {
+  printf "\033[0;31m%s\033[0m\n" "$*" >&2
+}
+
+error() {
+  red "$*"
+  exit 1
+}
+
+error "
+⚠️ Detected old chezmoi version
+
+❌ Manual action needed. To continue, please run:
+
+👉 chezmoi upgrade && chezmoi init --apply
+"
