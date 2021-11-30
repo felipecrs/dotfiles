da0c705946cdce3aee294dbf49d7781534458237
Felipe Santos <felipecassiors@gmail.com>
2021-11-30 13:51:59 -0300

    Remove workarounds after chezmoi 2.9.1

--- install.sh
+++ install.sh
@@ -40,5 +40,4 @@ fi
 
 echo_task "Running chezmoi init"
 # replace current process with chezmoi init
-# /home is needed because of https://github.com/twpayne/chezmoi/issues/1657
-exec "${chezmoi}" init --source "${script_dir}/home" "${chezmoi_extra_arg}"
+exec "${chezmoi}" init --source "${script_dir}" "${chezmoi_extra_arg}"
