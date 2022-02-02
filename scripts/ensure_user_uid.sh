03506288e7f3b51baf02441c24b782e3422bb669
Felipe Santos <felipecassiors@gmail.com>
2022-02-02 19:43:51 -0300

    Add utility script to ensure user has a given uid

---
+++ scripts/ensure_user_uid.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+set -euxo pipefail
+
+# shellcheck disable=SC2312
+if [[ $(id -u) -ne 0 ]]; then
+  echo "This script must be run as root."
+  exit 1
+fi
+
+old_user="${1}"
+old_uid=$(id -u "${old_user}")
+new_uid="${2}"
+
+function get_user_name() {
+  id -nu "${1}"
+}
+
+function check_process_for_uid() {
+  local user_name
+  user_name=$(get_user_name "${1}")
+  # shellcheck disable=SC2009
+  if ps -ef | grep "^${user_name} "; then
+    echo "Above is the list of processes running for user ${user_name}, please log out."
+    return 1
+  fi
+}
+
+check_process_for_uid "${old_uid}"
+
+function relocate_user() {
+  local old=$1
+  local name
+  name=$(get_user_name "${old}")
+  local new=$2
+
+  # shellcheck disable=SC2312
+  if [[ $(id -ng "${old}") != "${name}" ]]; then
+    echo "This script only support relocating users with group that matches the uid and user name."
+    exit 1
+  fi
+
+  # shellcheck disable=SC2312
+  if [[ $(getent group "${name}" | wc -l) != "1" ]]; then
+    echo "This script only support relocating users with group that have only the same user inside."
+    exit 1
+  fi
+
+  check_process_for_uid "${old}"
+
+  id "${name}"
+
+  groupmod -g "${new}" "${name}"
+  usermod -u "${new}" -g "${new}" "${name}"
+
+  id "${name}"
+
+  find / -ignore_readdir_race -group "${old}" -user "${old}" -print0 | xargs -0 --no-run-if-empty chown -ch "${name}:${name}"
+  find / -ignore_readdir_race -group "${old}" -print0 | xargs -0 --no-run-if-empty chgrp -ch "${name}"
+  find / -ignore_readdir_race -user "${old}" -print0 | xargs -0 --no-run-if-empty chown -ch "${name}"
+
+  echo "Finished moving."
+}
+
+if existing_user=$(get_user_name "${new_uid}"); then
+  next_uid=$(cat /etc/group /etc/passwd | cut -d ":" -f 3 | grep "^1...$" | sort -n | tail -n 1 | awk '{ print $1+1 }')
+  echo "First moving existing user ${existing_user} to the next available UID/GID is ${next_uid}"
+
+  relocate_user "${new_uid}" "${next_uid}"
+fi
+
+echo "Moving now..."
+relocate_user "${old_uid}" "${new_uid}"
