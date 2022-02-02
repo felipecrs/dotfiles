8ab74ebf5e53f722928f564d2490ba63ed3b8a06
Felipe Santos <felipecassiors@gmail.com>
2022-02-02 17:12:41 -0300

    Remove --privileged flag

--- scripts/test.sh
+++ scripts/test.sh
@@ -107,7 +107,7 @@ run_test() {
   local -r os="$1"
   local -r setup_script="$2"
 
-  cmd time docker run --rm --init --privileged --interactive --user vscode \
+  cmd time docker run --rm --init --interactive --user vscode \
     --volume "${dotfiles_root}:/home/vscode/.dotfiles:ro" \
     "mcr.microsoft.com/vscode/devcontainers/base:${os}" \
     bash <<EOF
