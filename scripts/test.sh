c60711e78a2c645242f880ab17deb5618a9c6562
Felipe Santos <felipecassiors@gmail.com>
2022-02-02 17:48:53 -0300

    Restart docker daemon if needed (#67)
    But this still has the drawback of restarting the daemon when the common
    script changes. If it becomes an issue I can revisit this.
    
    Closes #66

--- scripts/test.sh
+++ scripts/test.sh
@@ -117,6 +117,9 @@ ${_arg_pre_script}
 
 ${setup_script}
 
+export DOTFILES_TEST=true
+echo 'Defaults env_keep += "DOTFILES_TEST"' | sudo tee /etc/sudoers.d/env_keep
+
 ~/.dotfiles/install.sh
 
 set +xeu
