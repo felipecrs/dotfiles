ece66ca52574d583d8ff17e712b9728b0c3d3ede
Felipe Santos <felipecassiors@gmail.com>
2023-02-13 11:45:07 -0300

    Fix wording

--- install.sh
+++ install.sh
@@ -53,7 +53,7 @@ script_dir="$(cd -P -- "$(dirname -- "$(command -v -- "$0")")" && pwd -P)"
 
 set -- init --source="${script_dir}" --verbose=false
 
-# If interactive, add TTY flag
+# If interactive, display progress bar
 if [ -t 0 ]; then
   set -- "$@" --progress
 fi
