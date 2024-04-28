36642e40a978d20b7b105e855f7ed5bf4d896476
Felipe Santos <felipecassiors@gmail.com>
2024-04-28 12:48:50 -0300

    Install pipx using pyz
    Because pipx on Ubuntu 20.04 is outdated and does not function well.

--- scripts/install_dotfiles.sh
+++ scripts/install_dotfiles.sh
@@ -78,7 +78,7 @@ DOTFILES_DIR="${HOME}/.dotfiles"
 if ! command -v git >/dev/null 2>&1; then
   log_task "Installing git"
   sudo apt update --yes
-  sudo apt install git --yes
+  sudo apt install --yes --no-install-recommends git
 fi
 
 if [ -d "${DOTFILES_DIR}" ]; then
